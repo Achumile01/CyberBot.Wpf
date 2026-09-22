a7aa126f34e3b9de3c35694e7d93b49a5f156134
Achumile01 <somdakaachumile727@gmail.com>
2026-09-22 21:18:42 +0200

    Update and rename SQLDB.sql to SQL/SQLDB.sql

--- SQL/SQLDB.sql
+++ SQL/SQLDB.sql
@@ -7,7 +7,7 @@ GO
 CREATE TABLE ActivityLog
 (
     ActivityId INT IDENTITY(1,1) PRIMARY KEY,
-    ActivityType NVARCHAR(50),
-    Description NVARCHAR(255),
+    ActivityType VARCHAR(50),
+    Description VARCHAR(255),
     ActivityDate DATETIME DEFAULT GETDATE()
 );
